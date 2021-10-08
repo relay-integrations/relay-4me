9c0235007f4eceebb509bbdfba7a93ed900c95c0
Dimitri Tischenko <dimitri@puppet.com>
2021-10-08 13:55:33 +0200

    add properties

--- steps/4me-requests-create/step.rb
+++ steps/4me-requests-create/step.rb
@@ -2,20 +2,11 @@
 require 'sdk4me/client'
 require 'net/http'
 
-def metadata_uri
-  endpoint = ENV['METADATA_API_URL']
-  uri = URI("#{endpoint}/spec")
-  uri
-end
-
 def send_relay_outputs(outputs)
-  puts outputs
   endpoint = ENV['METADATA_API_URL']
   uri = URI("#{endpoint}/outputs/request")
   req = Net::HTTP::Put.new(uri, { 'Content-Type' => 'application/json' })
   req.body = outputs.to_json
-  # req.body = outputs
-  puts "\n\n***body: #{req.body}"
   req.basic_auth uri.user, uri.password
   begin
     res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
@@ -25,46 +16,42 @@ def send_relay_outputs(outputs)
     puts "Error writing outputs: #{e}"
     raise e
   end
-  unless res.is_a? Net::HTTPSuccess
-    puts "Error writing outputs: #{res.inspect} #{res.body}"
-    exit(1)
-  end
+  return res if res.is_a? Net::HTTPSuccess
+
+  puts "Error writing outputs: #{res.inspect} #{res.body}"
+  exit(1)
 end
 
 def mock_data
   require 'yaml'
-  puts "in mock_data"
-  data = YAML.safe_load(File.read("test-metadata-local.yaml"))["runs"]["1"]["steps"]["default"]["spec"]
+  data = YAML.safe_load(File.read('test-metadata-local.yaml'))['runs']['1']['steps']['default']['spec']
   puts "data : #{data}"
   { value: data }
 end
 
 def relay_inputs
-  if ENV['RELAY_MOCK']
-    mock_data.deep_transform_keys(&:to_sym)
-  else
-    uri = metadata_uri
-    req = Net::HTTP::Get.new(uri)
-    req.basic_auth uri.user, uri.password
-    begin
-      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
-        http.request(req)
-      end
-    rescue StandardError => e
-      puts "Error fetching metadata: #{e}"
-      raise e
+  return mock_data.deep_transform_keys(&:to_sym) if ENV['RELAY_MOCK']
+
+  endpoint = ENV['METADATA_API_URL']
+  uri = URI("#{endpoint}/spec")
+  req = Net::HTTP::Get.new(uri)
+  req.basic_auth uri.user, uri.password
+  begin
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+      http.request(req)
     end
-    if res.is_a? Net::HTTPSuccess
-      JSON.parse(res.body).deep_transform_keys(&:to_sym)
-    else
-      puts "Error fetching metadata: #{res.inspect} #{res.body}"
-      exit(1)
-    end
+  rescue StandardError => e
+    puts "Error fetching metadata: #{e}"
+    raise e
   end
+  return JSON.parse(res.body).deep_transform_keys(&:to_sym) if res.is_a? Net::HTTPSuccess
+
+  puts "Error fetching metadata: #{res.inspect} #{res.body}"
+  exit(1)
 end
 
-log = Logger.new(STDOUT)
-log.level = Logger::INFO
+log = Logger.new($stdout)
+log.level = Logger::DEBUG
 
 spec = relay_inputs[:value]
 connection = spec[:connection]
@@ -78,27 +65,12 @@ end
 
 client = Sdk4me::Client.new
 response = client.get('me')
-# puts response[:primary_email]
-# puts JSON.pretty_generate(response.json)
+
 my_id = response[:id]
-
-# requests = client.each('requests') do |request|
-#  puts "#{request[:id]} #{request[:subject]}"
-# end
-
-# service_instances = client.each('service_instances') do |service_instance|
-# puts "#{service_instance[:id]} #{service_instance[:name]}"
-# end
 
 request = spec[:request]
 
-response = Sdk4me::Client.new.post('requests', {
-                                     requested_by: my_id,
-                                     category: request[:category],
-                                     subject: request[:subject],
-                                     impact: request[:impact],
-                                     service_instance: request[:service_instance_id]
-                                   })
+response = Sdk4me::Client.new.post('requests', request.merge({ requested_by: my_id }))
 
 if response.valid?
   puts "New request created with id #{response[:id]}"
